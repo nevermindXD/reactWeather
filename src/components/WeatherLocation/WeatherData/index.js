a87029c2d2702d1db97f735ea06b0f5d4a80e34f
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2018-02-06 18:29:15 -0600

    section 6, lecture 43

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfro from './WeatherExtraInfo';
-import {SNOW} from './../../../constants/weather';
 import './style.css';
 
-const WeatherData = () => (
+const WeatherData = ({ data }) => {
+    const { temperature, weatherState, humidity, wind } = data;
+    return(
     <div className='weatherDataCont'>
-        <WeatherTemperature temperature={20} weatherState={SNOW}/>
-        <WeatherExtraInfro humidity={80} wind={'10m/s'}/>
-    </div>
-);
+        <WeatherTemperature temperature={temperature} weatherState={weatherState}/>
+        <WeatherExtraInfro humidity={humidity} wind={wind}/>
+    </div>);
+};
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+}
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import { SUN } from './../../constants/weather';
 import './style.css';
 
+const location = 'Guadalajara, MX';
+const api_key = 'f0871dbe6cf411711e8ef3408b9a9add';
+const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
 
-const WeatherLocation = () =>(
-    <div className='weatherLocationCont'>
-        <Location city={'Mexico'}/>
-        <WeatherData/>
-    </div>
-);
+const data1 = {
+    temperature: 20,
+    weatherState: SUN,
+    humidity: 10, 
+    wind: '10 m/s'
+};
+
+
+class WeatherLocation extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            city: 'Guadalajara',
+            data: data1
+        };
+    }
+
+    handleUpdateClick = () => {
+        fetch(api_weather)
+            .then(data =>{
+                console.log(data);
+                return data.json();
+            }).then(weather_data=>{
+                console.log(weather_data);
+            })
+            .catch(e=>{
+                console.log(e);
+            })
+
+        /*
+        this.setState({
+            city:'Mexico',
+            data: data2
+        });*/
+        console.log('actualizado');
+    }
+
+    render = () =>{
+        const { city, data } = this.state;
+        return (
+            <div className='weatherLocationCont'>
+                <Location city={city}/>
+                <WeatherData data={data}/>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        );
+    };
+}
 
 export default WeatherLocation;
